6a0e3667456a8a8e475478b391fa0ef7668fbcb6
Xia <1539400726@qq.com>
2018-02-10 22:45:21 +0800

    add some files

--- pythoncoding/basic/pooled_processing.py
+++ pythoncoding/basic/pooled_processing.py
@@ -6,7 +6,7 @@ def long_time_task(name):
     start = time.time()
     time.sleep(random.random() * 3)
     end = time.time()
-    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+    print('Task %s runs %0.2f seconds.' % (name, (end-start)))
     
 if __name__=='__main__':
     print('Parent process %s.' % os.getpid())
